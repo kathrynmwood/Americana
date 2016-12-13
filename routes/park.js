d0a955591db54bd81fc5497174d3d66989648d39
Katie Wood <woodkm11@gmail.com>
2016-12-13 15:27:37 -0500

    Individual park pages populate.

--- routes/park.js
+++ routes/park.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var Park = require('../models/park.js');
 
-/* GET home page. */
-router.get('/:parkName', function(req, res, next) {
-  var parkName = req.params.parkName;
+// /* GET home page. */
+// router.get('/:parkName', function(req, res, next) {
+//   var parkName = req.params.parkName;
 
-parkSchema.find({parkName:parkName}, function(err,result){
-if(err){
-  return next(err);
-}else{
-res.render('park', {title: parkName, data: result});
-
-}
-
-})
-
-
+// SHOW
+router.get('/:id', function(req, res, next) {
+    Park.findById(req.params.id)
+        .then(function(park) {
+            if (!park) return next(makeError(res, 'Document not found', 404));
+            res.render('park', {
+                park: park
+            });
+        }, function(err) {
+            return next(err);
+        });
 });
 
 module.exports = router;
